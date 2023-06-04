5cdfa1e055ea01d08e18e757e3f14784ded0d285
DDDavidSt <david.stevanak@gmail.com>
2023-06-04 21:31:14 +0200

    tried plotly graph and chanegd results page a bit

--- flask_app/application.py
+++ flask_app/application.py
@@ -1,5 +1,8 @@
+import json
 from flask import Flask, render_template
-
+import pandas as pd
+import plotly
+import plotly.express as px
 app = Flask(__name__)
 
 
@@ -9,7 +12,23 @@ def home():
 
 @app.route('/results')
 def results():
-    return render_template("results.html")
+    df = pd.DataFrame({
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    })
+
+    fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    header="Fruit in North America"
+    description = """
+    A academic study of the number of apples, oranges and bananas in the cities of
+    San Francisco and Montreal would probably not come up with this chart.
+    """
+    return render_template('results.html', graphJSON=graphJSON, header=header,description=description)
+
+    # return render_template("results.html")
 
 @app.route('/tiy')
 def tyi():
